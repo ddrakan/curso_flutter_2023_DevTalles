3ba3201d277b81279f7ca123fdca8a0cb2d09aa4
ddrakan <ddrakan@hotmail.com>
2023-10-30 10:06:30 -0400

    - GetTheme implementado Se implementa el Darkmode en la toda la app. implement getTheme in app_theme.dart de manera general SelectColor & Darkmode -----------------------

--- widgets_app/lib/main.dart
+++ widgets_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/config/router/app_router.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
+import 'package:widgets_app/presentation/providers/theme_provider.dart';
 
 
 void main() {
@@ -12,17 +13,22 @@ void main() {
   );
 }
 
-class WidgetsApp extends StatelessWidget {
+class WidgetsApp extends ConsumerWidget {
   const WidgetsApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isDarkmode = ref.watch(isDarkmodeProvider);
+    final selectedColor = ref.watch(selectedColorProvider);
+
     return MaterialApp.router(
       title: 'Flutter Widget',
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 0, isDarkmode: true ).getTheme(),
+      theme: AppTheme(
+          selectedColor: selectedColor,
+          isDarkmode: isDarkmode ).getTheme(),
 
 
       // home: const HomeScreen(),
